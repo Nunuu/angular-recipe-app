62727e16cbb955f58b2991f889f09b4210fbb213
Ying He <ying@symbilityintersect.com>
2018-12-11 14:23:16 -0500

    replaced event emitters with observables

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,10 @@
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
 
-  ingredientsChanged = new EventEmitter<void>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -17,12 +17,12 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit();
+    this.ingredientsChanged.next();
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit();
+    this.ingredientsChanged.next();
   }
 
 }
